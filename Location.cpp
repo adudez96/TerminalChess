a7adab76d4d930afcb52fb95bc0187fefc97f360
Aman Singh <adudez96@gmail.com>
2017-12-27 14:51:33 +1100

    More editorconfig changes and newlines

--- Location.cpp
+++ Location.cpp
@@ -8,7 +8,7 @@ namespace chesslib {
 
 Location::Location(unsigned char f, unsigned char r): file{f}, rank{r} {
     if ((f < 'a' || f > 'h' || r < 1 || r > 8) && (f != 0 || r !=0)) {
-        throw std::invalid_argument("File must be a-h, and rank must be 1-8 (or 0,0 for captured)");
+        throw std::invalid_argument("File must be a-h, and rank must be 1-8 (or 0,0 for captured).");
     }
 }
 
